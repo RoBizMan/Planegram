540a842b2191b9a62105d1202c6bcd44821ee4da
Robert Tiffert <robert@tiffert.eu>
2024-08-19 17:44:25 +0000

    Add a detailed post page to allow users to view detailed post and like

--- gram/urls.py
+++ gram/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.grams_list, name='grams'),
+    path('', views.grams_list, name='grams'),  # List of all grams
+    path('<int:pk>/', views.gram_detail, name='gram_detail'),  # Detail view for a specific gram
 ]
--- gram/views.py
+++ gram/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from blog.models import Gram
-
 
 # Create your views here.
 
@@ -15,3 +14,17 @@ def grams_list(request):
     page_obj = paginator.get_page(page_number)
 
     return render(request, 'gram/grams_list.html', {'page_obj': page_obj})
+
+def gram_detail(request, pk):
+    # Retrieve the specific Gram object or return a 404 error if not found
+    gram = get_object_or_404(Gram, pk=pk)
+
+    if request.method == 'POST':
+        if 'like' in request.POST:
+            gram.love.add(request.user)
+        elif 'unlike' in request.POST:
+            gram.love.remove(request.user)
+        gram.save()
+        return redirect('gram_detail', pk=pk)
+
+    return render(request, 'gram/gram_detail.html', {'gram': gram})
